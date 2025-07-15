4d94294bb31ad08e9ae427e7332645c203e6c20d
Jacob Ng <ngjacob1@gmail.com>
2025-07-15 16:10:01 +1000

    Added telescope

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,12 @@
 local map = vim.keymap.set
 local opts = { noremap = true, silent = true }
+local builtin = require("telescope.builtin")
 
 -- Neotree
 vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>", { desc = "Toggle Neo-tree" })
 
+-- Telescope
+map("n", "<leader>ff", builtin.find_files, opts)
+map("n", "<leader>fg", builtin.live_grep, opts)
+map("n", "<leader>fb", builtin.buffers, opts)
+map("n", "<leader>fh", builtin.help_tags, opts)
---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,6 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  tag = "0.1.5",  -- stable release
+  dependencies = { "nvim-lua/plenary.nvim" },
+}
+
