17915b8fc6420d4ecc13051f5a1897f7b45559f5
Jacob Ng <ngjacob1@gmail.com>
2025-07-15 16:21:16 +1000

    Added lsp

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -10,3 +10,7 @@ map("n", "<leader>ff", builtin.find_files, opts)
 map("n", "<leader>fg", builtin.live_grep, opts)
 map("n", "<leader>fb", builtin.buffers, opts)
 map("n", "<leader>fh", builtin.help_tags, opts)
+
+-- LSP
+vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+vim.keymap.set("n", "K", vim.lsp.buf.hover)
---
+++ lua/plugins/lspconfig.lua
@@ -0,0 +1,24 @@
+return {
+  "neovim/nvim-lspconfig",
+  dependencies = {
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+  },
+  config = function()
+    require("mason").setup()
+    require("mason-lspconfig").setup({
+      ensure_installed = { "lua_ls", "ts_ls", "pyright" }, 
+      automatic_installation = true,
+    })
+
+    local lspconfig = require("lspconfig")
+    local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+    -- Setup LSP servers
+    lspconfig.lua_ls.setup({ capabilities = capabilities })
+    lspconfig.ts_ls.setup({ capabilities = capabilities })
+    lspconfig.pyright.setup({ capabilities = capabilities })		--
+
+  end,
+}
+
