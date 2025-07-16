e4c466d3f73720b3a1a753c531bce51f278169b4
Jacob Ng <ngjacob1@gmail.com>
2025-07-16 18:59:33 +1000

    Added session

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,12 +1,17 @@
 return {
   "nvim-telescope/telescope.nvim",
   tag = "0.1.5",  -- stable release
-  dependencies = { "nvim-lua/plenary.nvim" },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope-frecency.nvim",
+    "tami5/sqlite.lua",
+  },
   keys = {
     { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
+    { "<leader>fh", "<cmd>Telescope oldfiles<cr>", desc = "Recent Files" },
+    { "<leader>fr", "<cmd>Telescope frecency<cr>", desc = "Frecency" },
     { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live Grep" },
-    { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "List Buffers" },
-    { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help Tags" },
+    { "<leader>fm", "<cmd>Telescope marks<cr>", desc = "Bookmarks" },
   },
 }
 
