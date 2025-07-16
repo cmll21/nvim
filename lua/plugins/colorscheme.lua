c17f497c779461290ce9731c11d167be99d1c3a3
Jacob Ng <ngjacob1@gmail.com>
2025-07-16 14:21:35 +1000

    Added colorschemes and dashboard

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,11 @@
+return { 
+  {'projekt0n/github-nvim-theme', name = 'github-theme' },
+  {'Mofiqul/vscode.nvim', name = 'vscode-theme' },
+  {'navarasu/onedark.nvim', name = 'onedark-theme' },
+  {'catppuccin/nvim', name = 'catppuccin' },
+  {'sainnhe/everforest', name = 'everforest' },
+  {'sainnhe/gruvbox-material', name = 'gruvbox-material' },
+  {'folke/tokyonight.nvim', name = 'tokyonight' },
+  {'EdenEast/nightfox.nvim', name = 'nightfox' },
+  {'shaunsingh/nord.nvim', name = 'nord' }
+}
