7b81c783c4c42e461b7d85b8382a17f33b53cba5
Jacob Ng <ngjacob1@gmail.com>
2025-07-17 05:11:54 +1000

    Modified default colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,18 +2,17 @@ return {
   {'projekt0n/github-nvim-theme', name = 'github-theme' },
   {'Mofiqul/vscode.nvim', name = 'vscode-theme' },
   {'navarasu/onedark.nvim', name = 'onedark-theme' },
-  {'catppuccin/nvim', name = 'catppuccin' },
+  {'catppuccin/nvim', 
+    name = 'catppuccin',
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme('catppuccin')
+    end,
+  },
   {'sainnhe/everforest', name = 'everforest' },
   {'sainnhe/gruvbox-material', name = 'gruvbox-material' },
   {'folke/tokyonight.nvim', name = 'tokyonight' },
   {'EdenEast/nightfox.nvim', name = 'nightfox' },
   {'shaunsingh/nord.nvim', name = 'nord' },
-  {
-    'rebelot/kanagawa.nvim',
-    name = 'kanagawa',
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme('kanagawa')
-    end,
-  },
+  { 'rebelot/kanagawa.nvim', name = 'kanagawa' },
 }
