8c68bf0d486da003b6f547caf4b38db8aab0e805
Jacob Ng <ngjacob1@gmail.com>
2025-07-24 16:54:47 +1000

    updated neogit

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -10,26 +10,8 @@ return {
 	},
 	config = function()
 		require("neogit").setup({
-			-- open the main status buffer as a floating window
 			kind = "floating",
-			floating = {
-				relative = "editor",
-				border = "rounded",
-				style = "minimal",
-				width = 0.8,
-				height = 0.7,
-			},
-
-			-- commit editor in a floating window
-			commit_editor = {
-				kind = "floating",
-				size = {
-					width = 0.7,
-					height = 0.5,
-				},
-			},
-
-			-- force all other views into floaters
+			commit_editor = { kind = "floating" },
 			commit_view = { kind = "floating" },
 			log_view = { kind = "floating" },
 			rebase_editor = { kind = "floating" },
